97f6aef4a791f3d88dfb4c9b3a3df82c5412ef7f
sunnykarira <sunnykarira@outlook.com>
2016-02-28 19:40:43 +0530

    Add POST /users/login and GET /users/logout route

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var bcrypt = require('bcrypt');
+
+
+var mongo = require('mongodb');
+var User = require('../models/user.js');
+var db = require('monk')('localhost/portal');
+
+// require passport and local startegy
+var passport = require('passport');
+var localStrategy = require('passport-local').Strategy;
 
 /* GET users listing. */
 /*router.get('/', function(req, res, next) {
@@ -13,6 +23,9 @@ router.get('/login', function(req, res){
 	});
 });
 
+
+
+
 // GET /users/register
 router.get('/register', function (req, res){
 	res.render('register', {
@@ -22,5 +35,138 @@ router.get('/register', function (req, res){
 });
 
 
+// POST /users/login
+router.post('/register', function (req, res,next){
+	//Get the form values
+	var name = req.body.name;
+	var email = req.body.email;
+	var enrollment = req.body.enrollment;
+	var password = req.body.password;
+	var password2 = req.body.password2;
+
+
+
+		//Check for image field
+	if(req.files.profileimage){
+		console.log('Uploading file...');
+		//Getting file info
+		var profileImageOriginalName = req.files.profileimage.originalname;
+		var profileImageName = req.files.profileimage.name;
+		var profileImageMime = req.files.profileimage.mimetype;
+		var profileImagePath = req.files.profileimage.path;
+		var profileImageExt = req.files.profileimage.extension;
+		var profileImageSize = req.files.profileimage.size;
+	}else{
+		//Set a default image
+		var profileImageName = 'noimage.png';
+	}
+
+	//Form Validation
+	// Value and error
+	req.checkBody('name', 'Name field is required').notEmpty();
+	req.checkBody('email', 'Email field is required').notEmpty();
+	req.checkBody('email', 'Email not valid').isEmail();
+	req.checkBody('enrollment', 'Enrollment Number is required').notEmpty();
+	req.checkBody('password', 'Password field is required').notEmpty();
+	req.checkBody('password2', 'Password do not match').equals(req.body.password);
+
+	//Check for errors
+	var errors = req.validationErrors();
+
+	if(errors){
+		res.render('register', {
+			errors: errors,
+			name: name,
+			email: email,
+			enrollment: enrollment,
+			password: password,
+			password2: password2
+		});
+	}else{
+
+		var newUser = new User({
+			name: name,
+			email: email,
+			enrollment: enrollment,
+			password: password,
+			profileimage: profileImageName
+		});
+
+		// Create User
+		User.createUser(newUser, function (err, user){
+			if(err) throw err;
+			console.log(user);
+
+		});
+
+		//Success Message
+		req.flash('success', 'You are now registered and may log in');
+		res.location('/');
+		res.redirect('/');
+
+	}
+
+
+});
+
+
+passport.serializeUser(function(user, done){
+	done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done){
+	
+	User.getUserById(id, function(err, user){
+		done(err, user);
+	});
+});
+
+passport.use(new localStrategy(
+
+		// username, password, callback
+	function(enrollment, password, done){
+		User.getUserByEnrollment(enrollment, function(err, user){
+			if(err) throw err;
+			if(!user){
+				console.log('Unknown User');
+				return done(null, false, {
+					message: 'unknown User'
+				});
+			}
+
+			User.comparePassword(password, user.password, function (err, isMatch){
+				if(err) throw err;
+				if(isMatch){
+					return done(null, user);
+				}else{
+					console.log('Invalid password');
+					return done(null, false, {
+						message: 'Invalid Password'
+					})
+				}
+			})
+		});	
+	}
+));
+
+
+//Post login route
+router.post('/login', passport.authenticate('local', {
+	failureRedirect:'/users/login',
+	failureFlash:'Invalid Enrollment or Password'
+}), function (req, res, next){
+	console.log('Authentication Successful');
+	req.flash('success', 'You are logged in');
+	res.location('/');
+	res.redirect('/');
+});
+
+
+router.get('/logout', function (req, res, next){
+	req.logout();
+	req.flash('success', 'You have logged out successfully');
+	res.location('/');
+	res.redirect('/');
+});
 
 module.exports = router;
