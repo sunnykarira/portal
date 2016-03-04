e7764668722f7067e800aa2b685d7a16f848a27b
sunnykarira <sunnykarira@outlook.com>
2016-03-04 16:37:25 +0530

    Add GET / and GET /home route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+function ensureAuthenticated(req, res, next){
+	//Passport Authentication API
+	if(req.isAuthenticated()){
+		return next();
+	}	
+	res.redirect('/home');
+}
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', ensureAuthenticated, function(req, res, next) {
+	res.render('index', { title: 'Express' });
+  
+});
+
+
+router.get('/home', function(req, res, next){
+	res.render('outerindex', {
+		title: 'OuterIndex'
+	})
 });
 
 module.exports = router;
