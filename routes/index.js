7afc693709b8db35bce457f106aa42481d0f3c22
sunnykarira <sunnykarira@outlook.com>
2016-03-09 20:08:41 +0530

    Add POST / route to update profile

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var bcrypt = require('bcrypt');
+var moment = require('moment');
+
+
+var mongo = require('mongodb');
+var User = require('../models/user.js');
+var db = require('monk')('localhost/portal');
+
+// require passport and local startegy
+var passport = require('passport');
+var localStrategy = require('passport-local').Strategy;
 
 function ensureAuthenticated(req, res, next){
 	//Passport Authentication API
@@ -11,7 +22,11 @@ function ensureAuthenticated(req, res, next){
 
 /* GET home page. */
 router.get('/', ensureAuthenticated, function(req, res, next) {
-	res.render('index', { title: 'Express' });
+	var user = req.user;
+	res.render('index', { 
+		title: 'Express',
+		user: user 
+	});
   
 });
 
@@ -21,5 +36,84 @@ router.get('/home', function(req, res, next){
 		title: 'OuterIndex'
 	})
 });
+
+router.post('/', function (req, res, next){
+	//Get the form values
+	var name = req.body.name;
+	var email = req.body.email;
+	var username = req.body.username;
+	var branch = req.body.branch;
+	var semester = req.body.semester;
+	var password = req.body.password;
+	var password2 = req.body.password2;
+
+
+		//Check for image field
+	if(req.files.profileimage){
+		console.log('Uploading file...');
+		//Getting file info
+		var profileImageOriginalName = req.files.profileimage.originalname;
+		var profileImageName = req.files.profileimage.name;
+		var profileImageMime = req.files.profileimage.mimetype;
+		var profileImagePath = req.files.profileimage.path;
+		var profileImageExt = req.files.profileimage.extension;
+		var profileImageSize = req.files.profileimage.size;
+	}else{
+		//Set a default image
+		var profileImageName = 'noImage.png';
+	}
+
+	//Form Validation
+	// Value and error
+	req.checkBody('name', 'Name field is required').notEmpty();
+	req.checkBody('email', 'Email field is required').notEmpty();
+	req.checkBody('email', 'Email not valid').isEmail();
+	req.checkBody('username', 'Username field is required').notEmpty();
+	req.checkBody('password', 'Password field is required').notEmpty();
+	req.checkBody('password2', 'Password do not match').equals(req.body.password);
+
+	//Check for errors
+	var errors = req.validationErrors();
+
+	if(errors){
+		res.render('index', {
+			errors: errors,
+			name: name,
+			email: email,
+			username: username,
+			password: password,
+			password2: password2
+		});
+	}else{
+
+		var users = db.get('users');
+		bcrypt.hash(password, 10, function (err, hash){
+		if(err) throw err;
+		//Set hashed password
+		password = hash;
+		users.update({
+			username: username
+
+		}, {
+			$set: {
+				password: password,
+				profileimage: profileImageName,
+				email: email
+			}
+		}, function(err, user){
+			if(err) throw(err);
+			req.flash('success','Profile updated, You may log in');
+			res.location('/');
+			res.redirect('/');
+			
+		});
+
+	});
+		
+		
+	}
+});
+
+
 
 module.exports = router;
